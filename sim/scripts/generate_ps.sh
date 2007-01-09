9ff4ab3636a91499f3c9ac93a4db947c39ed814f
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2007-01-09 12:47:35 +0000

    remove option

--- sim/scripts/generate_ps.sh
+++ sim/scripts/generate_ps.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-neato -Gstart=rand -Tps images/start -o images/a_start.ps
+neato -Tps images/start -o images/a_start.ps
 for I in images/frame* 
 do
-  neato -Gstart=rand -Tps $I -o $I.ps
+  neato -Tps $I -o $I.ps
 done
-neato -Gstart=rand -Tps images/end -o images/z_end.ps
+neato -Tps images/end -o images/z_end.ps
 
 
