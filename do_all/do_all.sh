5708fe6f914f43d6f3182176a9a4d187cb4d40fc
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2007-01-18 11:20:05 +0000

    script combinig ode solver and simulation

---
+++ do_all/do_all.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+. $CODEDIR/do_all/config
+
+usage="Usage: do_all.sh file_id [-m model_file] [-o ode_file]"
+usage="$usage [-s sim_file] [-n num_sims] [-d timestep] [model_options]"
+
+file_id=$1
+output_dir="$DATADIR/$file_id"
+
+force=n
+
+if [ -z $file_id ]; then
+  echo "Error: file_id not set"
+  echo "$usage"
+  exit 1
+fi
+
+shift
+
+# parse argument list
+
+while [ $# -ge 1 ]; 
+do
+  
+  case $1 
+  in
+      -m) shift; model=$1;;
+      -o) shift; ode=$1;;
+      -s) shift; sim=$1;;
+      -n) shift; num_sims=$1;;
+      -d) shift; dt=$1;;
+      -f) force="y";;
+      -h) echo $usage; exit 0;;
+      *) options="$options $1";;
+  esac
+  shift
+
+done
+
+if [ -d $output_dir ]; then
+    if [ $force == "y" ]; then
+	echo "Overwriting contents of $output_dir"
+	rm -f $output_dir/images/*
+    else
+	echo "Error: $output_dir exists"
+	echo "Use -f to override"
+	exit 1
+    fi
+else
+    mkdir $output_dir
+    mkdir $output_dir/images
+fi
+
+if [ $num_sims -gt 8999 ]; then
+  echo "WARNING: num_sims > 8999, setting to 8999"
+  set num_sims=8999
+fi
+
+echo "Running $num_sims simulations..."
+
+sim_options=""
+
+if [ $model ]; then
+    sim_options="$sim_options -m $model"
+fi
+if [ $sim ]; then
+    sim_options="$sim_options -p $sim"
+fi
+
+sim_base="$CODEDIR/graph/simulate"
+sim_base="$sim_base --graph-dir $output_dir/images $sim_options"
+sim_base="$sim_base $options"
+sim_command="$sim_base --write-file $output_dir/$file_id""100"
+
+$sim_command
+
+if [ $num_sims -gt 1 ]; then
+    for ((i=101;i<(100+$num_sims);i++)); do
+	sim_command="$sim_base --write-file $output_dir/$file_id$i"
+        $sim_command
+    done
+fi
+
+echo "Averaging runs..."
+
+avg_command="$CODEDIR/graph/average_runs -d $dt -o $output_dir/$file_id"
+avg_command="$avg_command $output_dir/$file_id???.sim.dat"
+
+$avg_command
+
+rm $output_dir/$file_id???.sim.dat
+rm $output_dir/$file_id???.init
+
+echo "Running ode..."
+
+if [ $model ]; then
+  ode_options="$ode_options -m $model"
+fi
+if [ $ode ]; then
+  ode_options="$ode_options -p $ode"
+fi
+
+ode_command="$CODEDIR/ode/ode_solve --file_id $output_dir/$file_id"
+ode_command="$ode_command $ode_options $options"
+$ode_command
+
+echo "Making plots"
+plot_command="$CODEDIR/do_all/makeplot.bash $file_id"
+$plot_command
---
+++ do_all/makeplot.bash
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+gp_ode_script=$CODEDIR/ode/mfpa.gp
+gp_sim_script=$CODEDIR/graph/sim.gp
+
+fixf=$CODEDIR/ode/fix21.tex
+
+# check command-line args
+if [[ $# != 1 ]]; then
+    echo "Usage: ./makeplot.bash file_id"
+    echo "       file_id is missing."
+    exit 1
+fi
+
+output_dir=$DATADIR/$1
+
+# create gnuplot script for ode
+cat $output_dir/$1.gp > $output_dir/$1.ode.tmp.gp
+sed -e s:FILE_ID:$output_dir/$1:g $gp_ode_script >> $output_dir/$1.ode.tmp.gp
+
+# make figs
+gnuplot $output_dir/$1.ode.tmp.gp > /dev/null
+rm -f $output_dir/$1.ode.tmp.gp
+
+# ps -> eps
+ps2epsi mf.ps mf.eps
+ps2epsi pa.ps pa.eps
+ps2epsi pa1.ps pa1.eps
+ps2epsi pa2.ps pa2.eps
+ps2epsi pa3.ps pa3.eps
+ps2epsi pa4.ps pa4.eps
+ps2epsi pa5.ps pa5.eps
+ps2epsi pa6.ps pa6.eps
+ps2epsi pa7.ps pa7.eps
+ps2epsi pa8.ps pa8.eps
+ps2epsi Cxx1.ps Cxx1.eps
+ps2epsi Cxx2.ps Cxx2.eps
+ps2epsi Cxx3.ps Cxx3.eps
+ps2epsi Cxx4.ps Cxx4.eps
+
+# create gnuplot script for simulaion
+cat $output_dir/$1.gp > $output_dir/$1.sim.tmp.gp
+sed -e s:FILE_ID:$output_dir/$1:g $gp_sim_script >> $output_dir/$1.sim.tmp.gp
+
+# make figs
+gnuplot $output_dir/$1.sim.tmp.gp > /dev/null
+rm -f $output_dir/$1.sim.tmp.gp
+
+# ps -> eps
+ps2epsi sim.ps sim.eps
+ps2epsi pairs1.ps pairs1.eps
+ps2epsi pairs2.ps pairs2.eps
+ps2epsi pairs3.ps pairs3.eps
+ps2epsi pairs4.ps pairs4.eps
+ps2epsi pairs5.ps pairs5.eps
+ps2epsi pairs6.ps pairs6.eps
+ps2epsi pairs7.ps pairs7.eps
+ps2epsi pairs8.ps pairs8.eps
+
+# create ps files
+latex singlets.tex > /dev/null 
+dvips -q -o $output_dir/$1.singlets.ps singlets.dvi > /dev/null
+latex pairs.tex  > /dev/null 
+dvips -q -o $output_dir/$1.pairs.ps pairs.dvi > /dev/null
+
+# clean
+rm -f singlets.log singlets.aux singlets.dvi 
+rm -f pairs.log pairs.aux pairs.dvi 
+rm -f mf.ps  mf.eps 
+rm -f pa.ps  pa.eps
+rm -f pa1.ps pa1.eps 
+rm -f pa2.ps pa2.eps 
+rm -f pa3.ps pa3.eps 
+rm -f pa4.ps pa4.eps
+rm -f pa5.ps pa5.eps 
+rm -f pa6.ps pa6.eps 
+rm -f pa7.ps pa7.eps 
+rm -f pa8.ps pa8.eps
+rm -f Cxx1.ps Cxx1.eps
+rm -f Cxx2.ps Cxx2.eps
+rm -f Cxx3.ps Cxx3.eps
+rm -f Cxx4.ps Cxx4.eps
+rm -f sim.ps sim.eps
+rm -f pairs1.ps pairs1.eps
+rm -f pairs2.ps pairs2.eps
+rm -f pairs3.ps pairs3.eps
+rm -f pairs4.ps pairs4.eps
+rm -f pairs5.ps pairs5.eps
+rm -f pairs6.ps pairs6.eps
+rm -f pairs7.ps pairs7.eps
+rm -f pairs8.ps pairs8.eps
+
+# extracting single figures
+#for fig in mf.eps pa.eps pa5.eps pa6.eps Cxx.eps
+#do
+#    echo "... $fig"
+#    sed -e s/FIGURE/$fig/g $fixf > tmp.tex
+#    latex tmp.tex > /dev/null  
+#    dvips -q -o tmp.ps tmp.dvi > /dev/null
+#    ps2epsi tmp.ps $1.$fig
+#    rm -f tmp.dvi tmp.log tmp.aux tmp.ps tmp.tex $fig
+#done	
+
+# ghostview
+gv $output_dir/$1.pairs.ps &
+gv $output_dir/$1.singlets.ps &
