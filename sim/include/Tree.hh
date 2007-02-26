511bd1303044ed877840c6cadb52e33568032567
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2007-02-26 13:39:16 +0000

    put functions to end of file

--- sim/include/Tree.hh
+++ sim/include/Tree.hh
@@ -14,97 +14,105 @@
 template <class T>
 class Tree
 {
-   public:
-      Tree(): lastLeaf(0), topBin(0) {;}
 
-      /******************************************************************/
-      // Tree::pickRandomElement
-      // picks a random vertex from the tree, based on a random number
-      // between 0 and 1
-      /******************************************************************/
-      T* pickRandomElement(double& randNo)
-      {
-         // check if we have a topBin (if not we either have no vertex
-         // or something else is seriously wrong)
-         if (topBin) {
-            // get the sum of the two slots for the topBin, corresponding
-            // to the total sum of rates of the whole tree
-            double rateSum = topBin->getRateSum();
-            if (rateSum > 0.) {
-               // call pickBin of the topBin, which will traverse down the tree
-               // using randNo, store the vertex eventually picked in eventBin
-               // and make randNo a random number in [0, eventBin->rateSum),
-               // which the leafBin can use to pick its own event
-               randNo *= rateSum;
+public:
+  Tree(): lastLeaf(0), topBin(0) {;}
+  
+  T* pickRandomElement(double& randNo);
 
-               Leaf<T>* pickedLeaf =
-                  dynamic_cast<Leaf<T>*>(topBin->pickBin(randNo));
-               if (pickedLeaf) {
-                  return pickedLeaf->getItem();
-               } else {
-                  std::cout << "picked bin is not a leaf, very strange..."
-                            << std::endl;
-               }
-            } else {
-               std::cout << "nothing can happen" << std::endl;
-            }
-         } else {
-            std::cout << "Could not find top bin." << std::endl;
-         }
-         // we should never end up here
-         return 0;
-      }
+  Bin* getTopBin() { return topBin; }
 
-      Bin* getTopBin() { return topBin; }
-      
-      Leaf<T>* lastLeaf; // latest leaf added of the tree
+  void addLeaf(double rateSum, T leafItem);
+  std::vector<Leaf<T>*>& getLeaves() { return leaves; }
+  
+private:
+  Leaf<T>* lastLeaf; // latest leaf added of the tree
+  Bin* topBin; // top Bin of the tree
+  std::vector<Leaf<T>*> leaves;   // reverse map from elements to leaves
 
-      // reverse map from elements to leaves
-      std::vector<Leaf<T>*> leaves;
-
-      /******************************************************************/
-      // Tree::addLeaf
-      // adds an item to the Tree
-      /******************************************************************/
-      void addLeaf(double rateSum, T leafItem)
-      {
-         // create the new leaf with given elementNumber (position in the
-         // vector) 
-         Leaf<T>* newLeaf =
-            new Leaf<T>(leafItem);
-         if (lastLeaf) {
-            // we are not creating the first vertex, so we try to an empty slot
-            // in the tree
-      
-            // call generateBinEntry for the Bin the lowest Bin is sitting in.
-            // This traverses the tree and accomodates gVertex in an empty slot
-            // and returns 0 or, if it cannot find an empty slot, creates a new
-            // top bin and a branch stretching down to gVertex, returning a
-            // pointer to the new top bin
-            Bin* newTop =
-               lastLeaf->getParent()->generateBinEntry(*newLeaf);
-            // if generateBinEntry returned non-zero we have a new top bin
-            if (newTop) {
-               topBin = newTop;
-            }
-         } else {
-            // no vertex exists yet, so create the first one
-            Bin* newBin = new Bin();
-            // add gVertex to the new bin and
-            // set the new bin as top-level bin for gVertex
-            newBin->addChild(*newLeaf);
-            newLeaf->setParent(newBin);
-            // the first bin is also the first top bin
-            topBin = newBin;
-         }
-         // set the new leaf to be the new last leaf
-         lastLeaf = newLeaf;
-         leaves.push_back(newLeaf);
-         return;
-      }
-   private:
-      Bin* topBin; // top Bin of the tree
 };
+
+/******************************************************************/
+// Tree::pickRandomElement
+// picks a random vertex from the tree, based on a random number
+// between 0 and 1
+/******************************************************************/
+
+template <class T>  
+T* Tree<T>::pickRandomElement(double& randNo)
+{
+  // check if we have a topBin (if not we either have no vertex
+  // or something else is seriously wrong)
+  if (topBin) {
+    // get the sum of the two slots for the topBin, corresponding
+    // to the total sum of rates of the whole tree
+    double rateSum = topBin->getRateSum();
+    if (rateSum > 0.) {
+      // call pickBin of the topBin, which will traverse down the tree
+      // using randNo, store the vertex eventually picked in eventBin
+      // and make randNo a random number in [0, eventBin->rateSum),
+      // which the leafBin can use to pick its own event
+      randNo *= rateSum;
+      
+      Leaf<T>* pickedLeaf =
+        dynamic_cast<Leaf<T>*>(topBin->pickBin(randNo));
+      if (pickedLeaf) {
+        return pickedLeaf->getItem();
+      } else {
+        std::cout << "picked bin is not a leaf, very strange..."
+                  << std::endl;
+      }
+    } else {
+      std::cout << "nothing can happen" << std::endl;
+    }
+  } else {
+    std::cout << "Could not find top bin." << std::endl;
+  }
+  // we should never end up here
+  return 0;
+}
+
+/******************************************************************/
+// Tree::addLeaf
+// adds an item to the Tree
+/******************************************************************/
+template <class T>
+void Tree<T>::addLeaf(double rateSum, T leafItem)
+{
+  // create the new leaf with given elementNumber (position in the
+  // vector) 
+  Leaf<T>* newLeaf =
+    new Leaf<T>(leafItem);
+  if (lastLeaf) {
+    // we are not creating the first vertex, so we try to an empty slot
+    // in the tree
+    
+    // call generateBinEntry for the Bin the lowest Bin is sitting in.
+    // This traverses the tree and accomodates gVertex in an empty slot
+    // and returns 0 or, if it cannot find an empty slot, creates a new
+    // top bin and a branch stretching down to gVertex, returning a
+    // pointer to the new top bin
+    Bin* newTop =
+      lastLeaf->getParent()->generateBinEntry(*newLeaf);
+    // if generateBinEntry returned non-zero we have a new top bin
+    if (newTop) {
+      topBin = newTop;
+    }
+  } else {
+    // no vertex exists yet, so create the first one
+    Bin* newBin = new Bin();
+    // add gVertex to the new bin and
+    // set the new bin as top-level bin for gVertex
+    newBin->addChild(*newLeaf);
+    newLeaf->setParent(newBin);
+    // the first bin is also the first top bin
+    topBin = newBin;
+  }
+  // set the new leaf to be the new last leaf
+  lastLeaf = newLeaf;
+  leaves.push_back(newLeaf);
+  return;
+}
 
 /******************************************************************/
 // generateTree
--- sim/include/Vertex.hh
+++ sim/include/Vertex.hh
@@ -12,79 +12,34 @@
 #include "Model.hh"
 
 class Edge {
-   public:
-      Edge(): type(Disease) {;}
-      Edge(EdgeType eType): type(eType) {;}
-      EdgeType type;
+
+public:
+
+  Edge(): type(Disease) {;}
+  Edge(EdgeType eType): type(eType) {;}
+  EdgeType type;
+
 };
 
 class Vertex {
-
-   public:
-
-      Vertex() : state(VertexState(Susceptible, Uninformed)) {;}
-      Vertex(int diseaseState, int infoState)
-         : state(VertexState(diseaseState, infoState)) {;}
-
-      Vertex(VertexState vs)
-         : state(vs) {;}
-      
-      virtual ~Vertex() {;}
-
-      VertexState state; // present (disease and information) state
-      double rateSum; // sum of the rates of all events that can
-                      // affect the vertex (change its state)
-
-      eventList events; // list of events associated with vertex
-
+  
+public:
+  
+  Vertex() : state(VertexState(Susceptible, Uninformed)) {;}
+  Vertex(int diseaseState, int infoState)
+    : state(VertexState(diseaseState, infoState)) {;}
+  
+  Vertex(VertexState vs)
+    : state(vs) {;}
+  
+  virtual ~Vertex() {;}
+  
+  VertexState state; // present (disease and information) state
+  double rateSum; // sum of the rates of all events that can
+  // affect the vertex (change its state)
+  
+  eventList events; // list of events associated with vertex
+  
 };
-
-
-/******************************************************************/
-// generateEventList function
-// generates an event list for a given vertex in a given graph,
-// using a given model. The vertices of the event must be of type
-// Vertex, otherwise this will throw a compile error
-/******************************************************************/
-template <class Graph, class VertexClass>
-double generateEventList(Graph& graph, VertexClass v,
-                         const Model& model)
-{
-   // definitions of boost types for quick access
-   typedef typename boost::graph_traits<Graph>::out_edge_iterator
-      out_edge_iterator;
-   typedef typename boost::graph_traits<Graph>::edge_descriptor
-      edge_descriptor;
-   typedef typename boost::graph_traits<Graph>::vertex_descriptor
-      vertex_descriptor;
-
-   // temporary sum for the new sum of rates of all events
-   // that can affect the vertex v
-   double tempSum = .0;
-
-   // clear event list
-   graph[v].events.clear();
-   // get node events
-   tempSum += model.getNodeEvents(graph[v].events, graph[v].state);
-
-   // get edge events
-   out_edge_iterator oi, oi_end;;
-
-   for (tie(oi, oi_end) = boost::out_edges(v, graph);
-        oi != oi_end; ++oi) {
-      edge_descriptor e = *oi;
-      vertex_descriptor t =  target(e, graph);
-      tempSum +=
-         model.getEdgeEvents(graph[v].events, graph[v].state,
-                             graph[e].type, graph[t].state);
-   }
-   
-   // calculate difference between new and old sum of rates
-   double diff = tempSum - graph[v].rateSum;
-   // set rateSum to new value
-   graph[v].rateSum = tempSum;
-
-   return diff;
-}
 
 #endif
