236e630dd034c2b44e340d2c1c8c21d733b0fdf4
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2007-04-19 11:43:17 +0000

    adopt to change in naming of image files

--- network/scripts/generate_gif.sh
+++ network/scripts/generate_gif.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 rm images/*.gif
-neato -Tgif images/start -o images/a_start.gif
 for I in images/frame* 
 do
   echo $I
   neato -Tgif $I -o $I.gif
 done
-neato -Tgif images/end -o images/z_end.gif
 
 
