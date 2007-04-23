ab457424ca4d170721751fb117aafe8d20750616
erez <>
2007-04-23 12:46:33 +0000

    implement random regular graphs

--- network/include/graph_structure.hh
+++ network/include/graph_structure.hh
@@ -7,98 +7,179 @@
 #define GENERATE_GRAPH_HH
 
 #include <boost/graph/graph_traits.hpp>
+#include <boost/random.hpp>
+#include <vector>
+#include <sys/time.h>
 
 //----------------------------------------------------------
 
 namespace boost {
-   template <typename Graph>
-   void add_vertices(Graph& g, 
-                     typename graph_traits<Graph>::vertices_size_type n)
-   {
-      for (unsigned int i = 0; i < n; i++) {
-         add_vertex(g);
-      }
-   }
-   
-   //----------------------------------------------------------
-   
-   template <typename Graph>
-   void add_vertices(Graph& g, 
-                     typename graph_traits<Graph>::vertices_size_type n,
-                     const typename vertex_property_type<Graph>::type v)
-   {
-      for (unsigned int i = 0; i < n; i++) {
-         add_vertex(v, g);
-      }
-   }
-   
-   //----------------------------------------------------------
-   
-   template <typename Graph, typename EdgeIterator>
-   void add_edge_structure(Graph& g, EdgeIterator ei, EdgeIterator ei_end)
-   {
-      while (ei != ei_end) {
-         add_edge((*ei).first, (*ei).second, g);
-         ++ei;
-      }
-   }
 
-   //----------------------------------------------------------
+  template <typename Graph>
+  void add_vertices(Graph& g, 
+                    typename graph_traits<Graph>::vertices_size_type n)
+  {
+    for (unsigned int i = 0; i < n; i++) {
+      add_vertex(g);
+    }
+  }
    
-   template <typename Graph, typename EdgeIterator>
-   void add_edge_structure(Graph& g, EdgeIterator ei, EdgeIterator ei_end,
-                           const typename edge_property_type<Graph>::type e)
-   {
-      while (ei != ei_end) {
-         add_edge((*ei).first, (*ei).second, e, g);
-         ++ei;
-      }
-   }
-
-   //----------------------------------------------------------
+  //----------------------------------------------------------
    
-   template <typename Graph>
-   unsigned int mark_parallel_edges(Graph& g)
-   {
-      typename graph_traits<Graph>::edge_iterator ei, ei_end;
-      typename graph_traits<Graph>::out_edge_iterator oi, oi_end;
-      typename Graph::vertex_descriptor s, t;
+  template <typename Graph>
+  void add_vertices(Graph& g, 
+                    typename graph_traits<Graph>::vertices_size_type n,
+                    const typename vertex_property_type<Graph>::type v)
+  {
+    for (unsigned int i = 0; i < n; i++) {
+      add_vertex(v, g);
+    }
+  }
+   
+  //----------------------------------------------------------
+   
+  template <typename Graph, typename EdgeIterator>
+  void add_edge_structure(Graph& g, EdgeIterator ei, EdgeIterator ei_end)
+  {
+    while (ei != ei_end) {
+      add_edge((*ei).first, (*ei).second, g);
+      ++ei;
+    }
+  }
 
-      // counter
-      unsigned int parallel_edges = 0;      
+  //----------------------------------------------------------
+   
+  template <typename Graph, typename EdgeIterator>
+  void add_edge_structure(Graph& g, EdgeIterator ei, EdgeIterator ei_end,
+                          const typename edge_property_type<Graph>::type e)
+  {
+    while (ei != ei_end) {
+      add_edge((*ei).first, (*ei).second, e, g);
+      ++ei;
+    }
+  }
+
+  //----------------------------------------------------------
+   
+  template <typename Graph>
+  unsigned int mark_parallel_edges(Graph& g)
+  {
+    typename graph_traits<Graph>::edge_iterator ei, ei_end;
+    typename graph_traits<Graph>::out_edge_iterator oi, oi_end;
+    typename Graph::vertex_descriptor s, t;
+
+    // counter
+    unsigned int parallel_edges = 0;      
       
-      // loop over all edges
-      for (tie(ei, ei_end) = edges(g); ei != ei_end; ei++) {
-         // setting source and target vertices
-         s = source(*ei, g);
-         t = target(*ei, g);
+    // loop over all edges
+    for (tie(ei, ei_end) = edges(g); ei != ei_end; ei++) {
+      // setting source and target vertices
+      s = source(*ei, g);
+      t = target(*ei, g);
          
-         // check if more than one edge is going to the same target
-         // from the edge's source
+      // check if more than one edge is going to the same target
+      // from the edge's source
        
-         unsigned int parallel_outgoing = 0;
+      unsigned int parallel_outgoing = 0;
          
-         for (tie(oi, oi_end) = out_edges(s, g); oi != oi_end; oi++) {
-            if (target(*oi, g) == t) parallel_outgoing++;            
-         }
-         
-         if (parallel_outgoing > 1) {
-            
-            // count parallel edges
-            parallel_edges++;
-            
-            // mark both parallel edges
-            g[*ei].parallel = true;
-         }      
+      for (tie(oi, oi_end) = out_edges(s, g); oi != oi_end; oi++) {
+        if (target(*oi, g) == t) parallel_outgoing++;            
       }
+         
+      if (parallel_outgoing > 1) {
+            
+        // count parallel edges
+        parallel_edges++;
+            
+        // mark both parallel edges
+        g[*ei].parallel = true;
+      }      
+    }
       
-      // since we looped over all edges, we have counted parallels twice
-      parallel_edges = parallel_edges / 2;
+    // since we looped over all edges, we have counted parallels twice
+    parallel_edges = parallel_edges / 2;
       
-      return parallel_edges;
+    return parallel_edges;
       
-   }
-}
+  }
+
+  //----------------------------------------------------------
+  // Random Regular Graph
+  
+  bool suitable(std::vector<unsigned int>& stubs,
+                std::vector<std::vector<int> >& seen_edges)
+  {
+    typedef std::vector<unsigned int>::iterator iter;
+    
+    for (iter s = stubs.begin(); s != stubs.end(); s++) 
+      for (iter t = s + 1; t != stubs.end(); t++) 
+        if (seen_edges[*s][*t] == 0) return 1; // success
+    
+    return 0; //failure
+  }
+
+  //----------------------------------------------------------
+  
+  template <typename DistributionType>
+  int random_regular_graph(std::vector<std::pair<unsigned int, unsigned int> >& rrg_edges,
+                           const unsigned int d,
+                           const unsigned int N,
+                           DistributionType& uni_gen)
+  {
+    
+    // define seen_edges NxN zero matrix
+    std::vector<std::vector<int> > seen_edges(N, std::vector<int>(N, 0));
+    
+    // init seen_edges
+    for (unsigned int i = 0; i < N; ++i)
+      seen_edges[i][i] = 1;
+    
+    // define stubs
+    std::vector<unsigned int> stubs;
+    
+    // init stubs
+    for (unsigned int i = 0; i < N; ++i)
+      for (unsigned int j = 0; j < d; ++j)
+        stubs.push_back(i);
+    
+    
+    // construct random regular graph
+    while (stubs.size()) {
+      
+      // select source and target from stubs
+      unsigned int src = static_cast<unsigned int>(uni_gen() * stubs.size());
+      unsigned int trg = static_cast<unsigned int>(uni_gen() * stubs.size());
+      unsigned int source = stubs[src];
+      unsigned int target = stubs[trg];
+      
+      // check if pair is suitable
+      if ( (source != target) && !(seen_edges[source][target]) ) {
+        
+        // removing source and target from stubs
+        stubs.erase(stubs.begin() + src);
+        if (src > trg) {
+          stubs.erase(stubs.begin() + trg);
+        } else {
+          stubs.erase(stubs.begin() + trg - 1);
+        }
+        
+        // update seen_edges
+        seen_edges[source][target] = 1;
+        seen_edges[target][source] = 1;         
+        
+        // add edge to rrg_edges
+        rrg_edges.push_back(std::make_pair(source, target));
+        
+      } else { // check if suitable stubs left
+        
+        if (!suitable(stubs, seen_edges)) return 0; // failure - no more suitable pairs
+      }
+    }   
+    
+    return 1; // success
+  }
+
+} // namespace boost
 
 //----------------------------------------------------------
   
