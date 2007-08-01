748d0a58b2ad3f121cf5589194416b9987c76fe8
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2007-08-01 12:10:56 +0000

    generate random number faster

--- network/include/assortativity.hh
+++ network/include/assortativity.hh
@@ -84,12 +84,14 @@ namespace boost {
       edge_descriptor;
     typedef typename boost::graph_traits<Graph>::vertex_descriptor
       vertex_descriptor;
-    typedef typename boost::graph_traits<Graph>::out_edge_iterator
-      out_edge_iterator;
+    typedef typename boost::graph_traits<Graph>::edge_iterator
+      edge_iterator;
+    typedef typename boost::graph_traits<Graph>::edges_size_type
+      edges_size_type;
     typedef typename edge_property_type<Graph>::type
       edge_property_type;
     
-    const unsigned int max_steps = 10000;
+    const unsigned int max_steps = 100000;
     unsigned int steps = 0;
 
     boost::uniform_01<boost::mt19937, double> uni_gen(r);
@@ -104,51 +106,84 @@ namespace boost {
     double H = assortativity_hamiltonian(g, J, et, deg_type1, deg_type2);
 
     bool converged = (fabs(current_assortativity - J)/J < 0.1);
+
+    // we will need to choose a lot of random edges, so we do not want to use
+    // the slow boost::random_edge. Instead, we create a vector of edges from
+    // which we can pick elements quickly
+
+    std::vector<edge_descriptor> et_edges;
+
+    edge_iterator ei, ei_end;
     
-    for (unsigned int i = 0; (i < max_steps) && (!converged); i++) {
-//     for (unsigned int i = 0; !converged; i++) {
-      // choose two random edges of type deg_type2 which do not share a node for
+    for (tie(ei, ei_end) = edges(g); ei != ei_end; ei++) {
+      if (g[*ei].type == et) et_edges.push_back(*ei);
+    }
+
+    unsigned int nEdges = et_edges.size();
+    
+    uniform_int<> dist(0, 2*nEdges-1);
+    variate_generator<RandomGenerator&, uniform_int<> > rand_gen(r, dist);
+
+//     for (unsigned int i = 0; (i < max_steps) && (!converged); i++) {
+    for (unsigned int i = 0; !converged; i++) {
+      // choose two random edges of type et which do not share a node for
       // rewiring
+
+      edges_size_type n1, n2;
+    
       edge_descriptor edge1, edge2, rewired1, rewired2;
       vertex_descriptor source1, target1, source2, target2;
 
+      
       // choose first random edge for swapping
-      do {
-        edge1 = random_edge_undirected(g, r);
-      } while (g[edge1].type != et);
-      source1 = source(edge1, g);
-      target1 = target(edge1, g);
-
+      n1 = rand_gen();
+      if (n1 > nEdges-1) {
+        n1 -= nEdges;
+        source1 = target(et_edges[n1], g);
+        target1 = source(et_edges[n1], g);
+      } else {
+        source1 = source(et_edges[n1], g);
+        target1 = target(et_edges[n1], g);
+      }
+      
       // choose second random edge for swapping
       bool valid_swap;
       do {
-        valid_swap = false;
-        edge2 = random_edge_undirected(g, r);
-        source2 = source(edge2, g);
-        target2 = target(edge2, g);
-        // make sure that the edge has the correct type
-        valid_swap = (g[edge2].type == et);
+        n2 = rand_gen();
+        if (n2 > nEdges-1) {
+          n2 -= nEdges;
+          source2 = target(et_edges[n2], g);
+          target2 = source(et_edges[n2], g);
+        } else {
+          source2 = source(et_edges[n2], g);
+          target2 = target(et_edges[n2], g);
+        }
+      
+        valid_swap = (n1 != n2);
         
         // make sure we have 4 distinct vertices
-        valid_swap &= !(source1 == source2 || source1 == target2 ||
-                        source2 == target1 || target1 == target2);
+        valid_swap = !(source1 == source2 || source1 == target2 ||
+                       source2 == target1 || target1 == target2);
         
-        // make sure that the none of the possibly new edges is already there
-        if (edge(g, source1, target2, et).second ||
-            edge(g, source2, target1, et).second) {
-          valid_swap = false;
+        if (valid_swap) {
+          // make sure that the none of the possibly new edges is already
+          // there 
+          if (edge(g, source1, target2, et).second ||
+              edge(g, source2, target1, et).second) {
+            valid_swap = false;
+          }
         }
       } while (!valid_swap);
 
       // calculated hamiltonian as it would be after rewiring
-      double s1_d1 = out_degree_type(g, source(edge1, g), deg_type1);
-      double s1_d2 = out_degree_type(g, source(edge1, g), deg_type2);
-      double t1_d1 = out_degree_type(g, target(edge1, g), deg_type1);
-      double t1_d2 = out_degree_type(g, target(edge1, g), deg_type2);
-      double s2_d1 = out_degree_type(g, source(edge2, g), deg_type1);
-      double s2_d2 = out_degree_type(g, source(edge2, g), deg_type2);
-      double t2_d1 = out_degree_type(g, target(edge2, g), deg_type1);
-      double t2_d2 = out_degree_type(g, target(edge2, g), deg_type2);
+      double s1_d1 = out_degree_type(g, source1, deg_type1);
+      double s1_d2 = out_degree_type(g, source1, deg_type2);
+      double t1_d1 = out_degree_type(g, target1, deg_type1);
+      double t1_d2 = out_degree_type(g, target1, deg_type2);
+      double s2_d1 = out_degree_type(g, source2, deg_type1);
+      double s2_d2 = out_degree_type(g, source2, deg_type2);
+      double t2_d1 = out_degree_type(g, target2, deg_type1);
+      double t2_d2 = out_degree_type(g, target2, deg_type2);
 
       double sum = s1_d1*t2_d2 + t2_d1*s1_d2 + s2_d1*t1_d2 + t1_d1*s2_d2 -
         s1_d1*t1_d2 - t1_d1*s1_d2 - s2_d1*t2_d2 - t2_d1*s2_d2;
@@ -173,12 +208,14 @@ namespace boost {
       
       if (accept) {
         H = temp_H;
-        remove_edge(edge1, g);
-        remove_edge(edge2, g);
-        rewired1 =
+        remove_edge(et_edges[n1], g);
+        remove_edge(et_edges[n2], g);
+        et_edges[n1] =
           (add_edge(source1, target2, edge_property_type(et), g)).first;
-        rewired2 =
+        et_edges[n2] =
           (add_edge(source2, target1, edge_property_type(et), g)).first;
+        et_edges[n1+nEdges] = et_edges[n1];
+        et_edges[n2+nEdges] = et_edges[n2];
       }
       
       // check if assortativity is converging
@@ -188,9 +225,9 @@ namespace boost {
           assortativity(g, deg_type1, deg_type1, deg_type2);
 
         // less than 5% change in 10000 events
-        converged =
-          (fabs(new_assortativity - current_assortativity)/
-           current_assortativity < 0.05);
+//         converged =
+//           (fabs(new_assortativity - current_assortativity)/
+//            current_assortativity < 0.05);
 //         current_assortativity = new_assortativity;
 
         if (verbose >=1) {
@@ -203,7 +240,7 @@ namespace boost {
 
     if (verbose >= 1) {
       std::cout << "Rewiring done: assortativity "
-                << current_assortativity
+                << assortativity(g, deg_type1, deg_type1, deg_type2)
                 << " obtained after " << steps << " steps" << std::endl;
     }
   }
