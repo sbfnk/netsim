3a2c94befb97d6101955535c4a7e4142eddd994b
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2007-08-01 09:44:37 +0000

    update documentation

--- network/include/path_length.hh
+++ network/include/path_length.hh
@@ -56,6 +56,11 @@ namespace boost {
 
   /*! \brief Visitor for getting the distance to a number of vertices
 
+  This differs from the standard boost::distance_recorder by considering only
+  the vertices in a vertex_vector which is passed to the operator(). Once the
+  shortest paths for all the vertices in that vector have been found, an
+  exception is thrown which can be caught in the main routine.
+  
   \param[in] et The edge type to consider.
   \ingroup graph_statistics
   */
@@ -96,14 +101,15 @@ namespace boost {
       (pa_all, pa_found, vec);
   }
 
-  /*! \brief Calculate the average shortest path length.
+  /*! \brief Calculate the average shortest neighbour path length.
 
-  Calculates the average shortest path length for edges of a given type by
-  looping over all vertices and then using a breadth_first_search to record the
-  distances to all other edges.
+  Calculates the average shortest path length to neighbours over one edge type
+  when traversing the other edge type. If the two networks overlap, this will
+  give 1.
   
   \param[in] g The graph to examine.
-  \param[in] et The edge type to consider.
+  \param[in] et1 The edge type to pick the neighbours from.
+  \param[in] et2 The second edge type to calculate the shortest path length for.
   \ingroup graph_statistics
   */
 
@@ -117,6 +123,7 @@ namespace boost {
     typedef typename graph_traits<Graph>::vertex_iterator
       vertex_iterator;
 
+    // if both types are equal, the shortest path length is always one.
     if (et1.type == et2.type) {
       return 1.;
     }
@@ -129,18 +136,23 @@ namespace boost {
 
     vertex_iterator vi, vi_end;
 
+    // loop over all vertices
     for (tie(vi, vi_end) = vertices(g); vi != vi_end; vi++) {
 
       std::fill_n(d_nb, num_vertices(g), 0);
       std::fill_n(d, num_vertices(g), 0);
 
+      // store et1 neighbours in et1_neighbours
       std::vector<vertex_descriptor> et1_neighbours;
       typename graph_traits<Graph>::out_edge_iterator oi, oi_end;
       for (tie(oi, oi_end) = out_edges(*vi, g); oi != oi_end; oi++) {
         if (g[*oi].type == et1.type) et1_neighbours.push_back(target(*oi, g));
       }
-      
+
+      // if vertex has et1 neighbours, do a shortest path search on et2
       if (et1_neighbours.size() > 0) {
+        // breadth_first_search throws an exception as soon as all the shortest
+        // path lengths for all entries in et1_neighbours have been found
         try {
           breadth_first_search(g, *vi, et2,
             visitor(make_bfs_visitor(record_vertex_distances(d, d_nb,
@@ -148,6 +160,7 @@ namespace boost {
                                                              on_tree_edge()))));
         } catch (...) {}
 
+        // add shortest paths to distance_sum
         unsigned int nb_count = 0;
         for (unsigned int i = 0;
              nb_count < et1_neighbours.size() && i < num_vertices(g); ++i) {
@@ -160,6 +173,7 @@ namespace boost {
       }
     }
 
+    // calculate average
     if (distance_count > 0) return (distance_sum/distance_count);
     else return 0.;
   }
