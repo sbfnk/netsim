128031adaccc58d0b40564a02aeadcefc1411309
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2007-10-26 13:43:39 +0000

    allow other parameters than alpha

--- do_all/scan_vincent.pl
+++ do_all/scan_vincent.pl
@@ -4,14 +4,16 @@ use Getopt::Long;
 use File::Path;
 use File::Copy;
 
-my $low_alpha = .0;
-my $high_alpha = .0;
+my $low_param = .0;
+my $high_param = .0;
 my $low_beta = .0;
 my $high_beta = .0;
 my $step = .02;
 
-my $alpha_step;
+my $param_step;
 my $beta_step;
+
+my $param = "alpha";
 
 my $code_dir = @ENV{'CODEDIR'};
 my $data_dir = @ENV{'DATADIR'};
@@ -19,11 +21,12 @@ my $data_dir = @ENV{'DATADIR'};
 my $command = "$code_dir/do_all/do_stats.sh";
 
 GetOptions("step=f" => \$step,
-	   "alpha-step=f" => \$alpha_step,
+	   "param=s" => \$param,
+	   "alpha-step=f" => \$param_step,
 	   "beta-step=f" => \$beta_step);
 
 my $usage = "Usage: scan_vincent.pl low_beta high_beta ".
-  "low_alpha high_alpha [-s step] [-a alpha-step] [-b beta-step]\n";
+  "low_param high_param [-s step] [-p param] [-a param-step] [-b beta-step]\n";
 
 (@ARGV >= 4) || die ($usage);
 
@@ -34,17 +37,17 @@ $ha = shift(@ARGV);
 
 ($lb <= $hb) && (la <= hb) || die ($usage); 
 
-($alpha_step > 0) || ($alpha_step = $step);
+($param_step > 0) || ($param_step = $step);
 ($beta_step > 0) || ($beta_step = $step);
-($alpha_step > 0) && ($beta_step > 0) || die($usage);
+($param_step > 0) && ($beta_step > 0) || die($usage);
 
-print "Varying alpha in [$la,$ha) in steps of $alpha_step and beta in [$lb,$hb) in steps of $beta_step\n";
+print "Varying $param in [$la,$ha) in steps of $alpha_step and beta in [$lb,$hb) in steps of $beta_step\n";
 print "Parameters: @ARGV\n\n";
 
 for ($alpha = $la; $alpha <= $ha; $alpha += $alpha_step) {
   for ($beta = $lb; $beta <= $hb; $beta += $beta_step) {
-    print("alpha=$alpha beta=$beta\n");
-    $arguments = sprintf("--alpha=%.2f --beta=%.2f",$alpha,$beta);
+    print("$param=$alpha beta=$beta\n");
+    $arguments = sprintf("--$param=%.2f --beta=%.2f",$alpha,$beta);
     system("$command $arguments @ARGV");
   }
 }
