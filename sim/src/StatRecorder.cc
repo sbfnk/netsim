d24c465260e20e6f2f26aeea4cc1f0c810467cb5
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2009-02-27 16:38:47 +0000

    small correction

--- sim/src/StatRecorder.cc
+++ sim/src/StatRecorder.cc
@@ -6,7 +6,8 @@ std::string generateFileName(std::string nameBase, unsigned int id,
                              std::string ext)
 {
   std::stringstream s;
-  s << nameBase << std::setw(6) << std::setfill('0') << id << "." << ext;
+  s << nameBase << std::setw(6) << std::setfill('0') << id;
+  if (ext.size() > 0) s << "." << ext;
   return s.str();
 }
 
