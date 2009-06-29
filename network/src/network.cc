628dc7ae05ee0587945476926868e6a10c017116
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2009-06-29 12:56:56 +0000

    make 1 edge type default

--- network/src/network.cc
+++ network/src/network.cc
@@ -106,7 +106,7 @@ int main(int argc, char* argv[])
   /******************************************************************/
   std::string topology;
 
-  unsigned int nEdgeTypes = 2;
+  unsigned int nEdgeTypes = 1;
   unsigned int N = 0;
   unsigned int verbose = 0;
 
