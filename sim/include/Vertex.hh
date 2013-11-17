6f41072199413f79fce52ecb249248a9156386b9
Sebastian Funk <sf7@princeton.edu>
2013-11-17 16:54:54 +0100

    remove code fragments

--- sim/include/Vertex.hh
+++ sim/include/Vertex.hh
@@ -10,23 +10,20 @@
 
 //----------------------------------------------------------
 /*! \brief The state a vertex can assume
-  
+
 \ingroup models
 \ingroup gillespie_simulator
 */
 //! Type denoting the state of an individual
 class State {
-  
+
 public:
   /*! \brief Constructor.
       \param[in] b base state.
   */
-  // State(int b = 0) : state(b) {std::cout << "Default state const " << reinterpret_cast<void*>(this) << std::endl;}
   State(int b = 0) : state(b) {}
-  // virtual ~State() { std::cout << "delete " << reinterpret_cast<void*>(this) << std::endl;}
   virtual ~State() {}
 
-  // virtual State* clone() const { State * s = new State(*this); std::cout << "State::clone, returning " << reinterpret_cast<void*>(s) << " from " << reinterpret_cast<const void*>(this) << std::endl; return s; }
   virtual State* clone() const { State * s = new State(*this); return s; }
 
   //! Print a coloured letter representing the vertex state.
@@ -39,14 +36,14 @@ public:
 private:
 
   int state;
-  
+
 };
 
 std::ostream& operator<<(std::ostream& os, const State& s);
 
 //----------------------------------------------------------
 /*! \brief An event which can happen to a vertex.
-  
+
 \ingroup models
 \ingroup gillespie_simulator
 */
@@ -62,7 +59,7 @@ public:
   Event(unsigned int r = 0, State* s = 0, unsigned int n=0, unsigned int e=0) :
     rate(r), newState(s), nb(n), et(e)
   {;}
-  
+
   ~Event() { if (newState) delete newState; }
   Event(const Event& e)
   {
@@ -81,11 +78,11 @@ public:
 
   //! The rate at which an event occurs (depends on model parameters),
   //  multiplied by 10^5 for integer representation
-  unsigned int rate; 
+  unsigned int rate;
   //! The state an event will change a vertex to
-  State* newState; 
+  State* newState;
   //! The neighbour "responsible" for the event
-  unsigned int nb; 
+  unsigned int nb;
   //! The edge type over which event is transmitted (if applicable)
   unsigned int et;
 
@@ -97,14 +94,14 @@ typedef std::vector<Event> eventList;
 \ingroup gillespie_simulator
 */
 class Vertex
-{      
+{
 public:
 
   //! Constructor.
   Vertex()
     // : state(new State(0)) {std::cout << "New di vertex " << reinterpret_cast<void*>(this) << " with state " << reinterpret_cast<void*>(state) << std::endl;}
     : state(new State(0)) {}
-  
+
   /*! \brief Constructor.
     \param[in] s state initialiser.
   */
@@ -119,17 +116,17 @@ public:
   Vertex& operator=(const Vertex& v)
   // { std::cout << "= operator, assigning " << reinterpret_cast<const void*>(v.state) << " (vertex " << reinterpret_cast<const void*>(&v) << ") to " << reinterpret_cast<void*>(state) << " (vertex " << reinterpret_cast<void*>(this) << ")" << std::endl; if (&v != this) { if (state) {delete state;} state = v.state->clone();} return *this; }
   { if (&v != this) { if (state) {delete state;} state = v.state->clone();} return *this; }
-    
+
   //! Destructor
   // virtual ~Vertex() { std::cout << "Vertex delete " << reinterpret_cast<void*>(this) << std::endl; if (state) { delete state;}}
   virtual ~Vertex() { if (state) { delete state;}}
 
   void setStatePtr(State* newState) { if (state) delete state; state = newState; }
   State* state;
-  
+
   //! The sum of rates of all events that can change the state of the vertex.
   unsigned int rateSum;
-      
+
   eventList events; //!< A list of events associated with vertex.
 };
 
